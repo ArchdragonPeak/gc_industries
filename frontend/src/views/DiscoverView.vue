2cd662afccdcd124bcf6e7ca1abb8f8a3d3a0c99
ArchdragonPeak <erzdrachengipfel@gmail.com>
2024-06-10 23:54:33 +0200

    removed old code from discoverview

--- frontend/src/views/DiscoverView.vue
+++ frontend/src/views/DiscoverView.vue
@@ -1,25 +1,5 @@
 <template>
     <h1 class="title">Entdecken</h1>
-    <button @click="toggleForm">Neues Spiel hinzufügen</button>
-    <form v-if="showForm" @submit.prevent="postGame">
-      <div>
-        <label for="name">Name:</label>
-        <input type="text" id="name" v-model="name" required>
-      </div>
-      <div>
-        <label for="gameID">Game ID:</label>
-        <input type="number" id="gameID" v-model="gameID" required>
-      </div>
-      <div>
-        <label for="tags">Tags:</label>
-        <input type="text" id="tags" v-model="tags" required>
-      </div>
-      <div>
-        <label for="gamelogo">Game Logo URL:</label>
-        <input type="text" id="gamelogo" v-model="gamelogo" required>
-      </div>
-      <button type="submit">Spiel speichern</button>
-    </form>
     <TestGameList :games="games"></TestGameList>
   </template>
   
@@ -33,18 +13,10 @@
     },
     data() {
       return {
-        games: [],
-        name: '',
-        gameID: '',
-        tags: '',
-        gamelogo: '',
-        showForm: false
+        games: []
       };
     },
     methods: {
-      toggleForm() {
-        this.showForm = !this.showForm;
-      },
       async fetchGames() {
         try {
           const response = await fetch('http://localhost:3000/games');
@@ -57,36 +29,6 @@
         } catch (error) {
           console.error('Es gab ein Problem mit der Fetch-Operation:', error);
         }
-      },
-      async postGame() {
-        try {
-          const response = await fetch('http://localhost:3000/games', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              name: this.name,
-              gameID: this.gameID,
-              tags: this.tags,
-              gamelogo: this.gamelogo
-            })
-          });
-          if (!response.ok) {
-            throw new Error('Netzwerkantwort war nicht ok');
-          }
-          const data = await response.json();
-          console.log('Erfolgreich gesendet:', data);
-          this.fetchGames();
-          this.resetForm();
-          this.toggleForm();
-        } catch (error) {
-          console.error('Fehler bei der Anfrage:', error);
-        }
-      },
-      resetForm() {
-        this.name = '';
-        this.gameID = '';
-        this.tags = '';
-        this.gamelogo = '';
       }
     },
     mounted() {
@@ -104,6 +46,8 @@ h1.title {
     font-size: 50px;
     text-align: center;
 }
+
+/* possibly unnecessary */
 div.wrapper {
     width: 100%;
     height: 800px;
