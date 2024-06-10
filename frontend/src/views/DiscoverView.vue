97b5e9306cd15609fbbd3b6ba29c7d7a5da51b8c
ArchdragonPeak <erzdrachengipfel@gmail.com>
2024-06-10 18:09:32 +0200

    added fetch and add game functionality

--- frontend/src/views/DiscoverView.vue
+++ frontend/src/views/DiscoverView.vue
@@ -1,51 +1,99 @@
 <template>
     <h1 class="title">Entdecken</h1>
-    <TestGameList :games='games'></TestGameList>
-</template>
-
-<script>
-import TestGameList from '../components/TestGameList.vue'
-
-export default{
+    <button @click="toggleForm">Neues Spiel hinzufügen</button>
+    <form v-if="showForm" @submit.prevent="postGame">
+      <div>
+        <label for="name">Name:</label>
+        <input type="text" id="name" v-model="name" required>
+      </div>
+      <div>
+        <label for="gameID">Game ID:</label>
+        <input type="number" id="gameID" v-model="gameID" required>
+      </div>
+      <div>
+        <label for="tags">Tags:</label>
+        <input type="text" id="tags" v-model="tags" required>
+      </div>
+      <div>
+        <label for="gamelogo">Game Logo URL:</label>
+        <input type="text" id="gamelogo" v-model="gamelogo" required>
+      </div>
+      <button type="submit">Spiel speichern</button>
+    </form>
+    <TestGameList :games="games"></TestGameList>
+  </template>
+  
+  <script>
+  import TestGameList from '../components/TestGameList.vue';
+  
+  export default {
     name: 'DiscoverView',
     components: {
-        TestGameList
+      TestGameList
     },
     data() {
-        return {
-            games: [
-            {
-                "name":"Space Game","gameID":"1", "tags":"Space, Adventure","bewertung":"4.5", "gamelogo": "./img/rndicon1.webp"
-            },
-            {
-                "name":"Immortal Kombat","gameID":"2", "tags":"Action, Fighting","bewertung":"2.5", "gamelogo": "./img/rndicon2.webp"
-            },
-            {
-                "name":"Cyber Bugs 1984","gameID":"3", "tags":"Action, RPG","bewertung":"4.7", "gamelogo": "./img/rndicon3.webp"
-            },
-            {
-                "name":"Indana Joseph","gameID":"4", "tags":"Point-and-Click-Adventure","bewertung":"3.8", "gamelogo": "./img/rndicon4.webp"
-            },
-            {
-                "name":"Suka Striker","gameID":"5", "tags":"FPS, Multiplayer","bewertung":"2.9", "gamelogo": "./img/rndicon5.webp"
-            },
-            {
-                "name":"Space Game 2","gameID":"6", "tags":"Action","bewertung":"4.1", "gamelogo": "./img/rndicon1.webp"
-            },
-            {
-                "name":"Immortal Kombat 2","gameID":"7", "tags":"Action","bewertung":"4.5", "gamelogo": "./img/rndicon2.webp"
-            },
-            {
-                "name":"Cyber Bugs 1985","gameID":"8", "tags":"Action","bewertung":"4.5", "gamelogo": "./img/rndicon3.webp"
-            },
-            ]
+      return {
+        games: [],
+        name: '',
+        gameID: '',
+        tags: '',
+        gamelogo: '',
+        showForm: false
+      };
+    },
+    methods: {
+      toggleForm() {
+        this.showForm = !this.showForm;
+      },
+      async fetchGames() {
+        try {
+          const response = await fetch('http://localhost:3000/games');
+          if (!response.ok) {
+            throw new Error('Netzwerkantwort war nicht ok');
+          }
+          const data = await response.json();
+          this.games = data;
+          console.log(this.games);
+        } catch (error) {
+          console.error('Es gab ein Problem mit der Fetch-Operation:', error);
         }
+      },
+      async postGame() {
+        try {
+          const response = await fetch('http://localhost:3000/games', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              name: this.name,
+              gameID: this.gameID,
+              tags: this.tags,
+              gamelogo: this.gamelogo
+            })
+          });
+          if (!response.ok) {
+            throw new Error('Netzwerkantwort war nicht ok');
+          }
+          const data = await response.json();
+          console.log('Erfolgreich gesendet:', data);
+          this.fetchGames();
+          this.resetForm();
+          this.toggleForm();
+        } catch (error) {
+          console.error('Fehler bei der Anfrage:', error);
+        }
+      },
+      resetForm() {
+        this.name = '';
+        this.gameID = '';
+        this.tags = '';
+        this.gamelogo = '';
+      }
+    },
+    mounted() {
+      this.fetchGames();
     }
-
-    
-}
-
-</script>
+  };
+  </script>
 
 <style scoped>
 body {
