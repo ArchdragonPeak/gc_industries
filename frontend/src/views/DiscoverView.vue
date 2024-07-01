3b26adee4727ab760d5531b9f3d169460138f0a3
ArchdragonPeak <erzdrachengipfel@gmail.com>
2024-07-01 18:39:32 +0200

    fix: deleted unnecessary code

--- frontend/src/views/DiscoverView.vue
+++ frontend/src/views/DiscoverView.vue
@@ -31,7 +31,7 @@ export default {
         threshold: 0.5,
         keys: ['name']
       });
-      const results = fuse.search(this.input.toLowerCase());
+      const results = fuse.search(this.input);
       return results.map(result => result.item);
     }
   },
