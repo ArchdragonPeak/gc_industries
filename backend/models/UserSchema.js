37eb1d91ce4b1924b9e19dd54f412b4e7932f225
SharpZ11 <f.zastepinski@gmail.com>
2024-07-09 09:30:11 +0200

    feat: added change password profileview

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const authMiddleware = require('../middleware/authMiddleware');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const UserSchema = new mongoose.Schema({
@@ -17,7 +18,7 @@ UserSchema.virtual('id').get(function() {
 });
 
 UserSchema.pre('save', async function(next) {
-    if (this.isModified('password')) {
+    if (this.isModified('password') && this.isNew) {
         this.password = await bcrypt.hash(this.password, 10);
     }
     next();
